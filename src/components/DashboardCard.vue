7240af54a8bf1c63b492f3422f30f84167cbfa1f
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-27 18:39:22 +0200

    Style header title and set as prop

--- src/components/DashboardCard.vue
+++ src/components/DashboardCard.vue
@@ -1,23 +1,30 @@
 <template>
-    <div>
+    <div class="card">
+        <h3 class="card__title">{{title}}</h3>
 
     </div>
 </template>
 
 <script>
     export default {
+         props: ['title']
         
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 .card {
     background: #FFFFFF;
     box-shadow: 0 2px 30px 0 rgba(50,50,50,0.05);
     border-radius: 10px;
-    max-width: 420px;
-    max-height: 347px;
+    width: 420px;
+    height: 347px;
+}
+
+.card__title {
+    color: $grey;
+    margin: 25px;
 }
 
 </style>
