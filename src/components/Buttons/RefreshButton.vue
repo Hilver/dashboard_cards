1962f99c2667663b23856cfe815ec39dce9d356d
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-27 19:31:48 +0200

    Add refresh button

---
+++ src/components/Buttons/RefreshButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button :class="['oval--button']"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.oval--button {
+    background-color: #fff;
+    width: 20px;
+    height: 20px;
+    border: 3px solid $grey;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+</style>
