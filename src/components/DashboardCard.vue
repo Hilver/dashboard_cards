7a5763a50379ecb3b4f439dc0a6d21c895d65aee
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-27 18:02:19 +0200

    Set styles

---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+.card {
+    background: #FFFFFF;
+    box-shadow: 0 2px 30px 0 rgba(50,50,50,0.05);
+    border-radius: 10px;
+    max-width: 420px;
+    max-height: 347px;
+}
+
+</style>
