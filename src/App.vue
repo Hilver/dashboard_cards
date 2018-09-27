f5e02b66e5a83f30c3dc2b6e701335850865d28f
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-27 17:48:54 +0200

    Add vue-router, clean boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <div id="app">
-    
+  <div id="app">    
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'app',
-  components: {
-    
-  }
-}
-</script>
 
 <style lang="scss">
 @import './assets/css/font.css';
