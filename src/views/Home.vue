1dd48c31527b0fdc2859649f138820e0c2f0f0d5
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-28 15:34:22 +0200

    Create notification component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,29 @@
     <dashboard-card :title="'Device Temperature'" :onClick="setFilterTemp">
       <component :is="temp_chart"></component>
     </dashboard-card>
-    
-    
+    <dashboard-card :title="'Notifications'" :onClick="setFilterTemp">
+      <component :is="notifications"></component>
+    </dashboard-card>
   </div>
 </template>
 
 <script>
 import DashboardCard from '../components/DashboardCard.vue';
 import TempChart from '../components/Charts/TempLineChart.vue';
+import Notifications from '../components/Notifications/Notifications.vue';
 export default {  
   name: 'home',
   components: {
     DashboardCard,
-    TempChart
+    TempChart,
+    Notifications
   },
   data() {
     return { 
       temp_chart: 'temp-chart',
       minTemp: 30,
-      maxTemp: 45
+      maxTemp: 45,
+      notifications: 'notifications'
       };
   },
   methods: {
