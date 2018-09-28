732199e1d1547229eb640b1c1bef6664e5fcb3ac
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-28 15:29:15 +0200

    Create components, get data from vuex, display and styling components

---
+++ src/components/Notifications/Notification.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="notification__template">
+        <div class="divider notification__divider"></div>
+        <h5 class="notification__date">{{ date }}</h5>
+        <p class="notification__message">{{ message }}</p>
+        <button :class="['notification__button', 'notification__button--text', color]"><img class="notification__button--icon" :src="button_icon"> {{ button_content }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            date: String,
+            message: String,
+            button_content: String,
+            button_icon: String,
+            color: String
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.notification__template {
+    margin: 25px 20px;
+}
+
+.notification__date {
+    font-size: 11px;
+    color: $light-grey;
+    margin: 0px;
+}
+
+.notification__message {
+    font-size: 15px;
+    color: #000;
+    font-weight: bold;
+    margin-top: 5px;
+}
+
+.notification__button {
+    background-color: #fff;
+    border: none;
+    line-height: 15px;    
+    cursor: pointer;
+}
+
+.notification__button--icon {
+    width: 15px;
+    height: 15px;
+    vertical-align: middle;
+    margin-right: 8px;
+    margin-bottom: 3px;
+}
+
+.notification__button--text {
+    font-size: 15px;
+    font-weight: bold;
+}
+
+.notification__divider {
+    margin-bottom: 20px;
+}
+
+.blue {
+    color: $blue
+}
+
+.green {
+    color: $green
+}
+
+</style>
---
+++ src/components/Notifications/Notifications.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <notification v-for="notification in notificationsData" 
+        :key="notification.id" 
+        :date="notification.date" 
+        :message="notification.message" 
+        :button_content="notification.button_content"
+        :button_icon="notification.button_icon"
+        :color="notification.color"        
+        ></notification>
+    </div>
+</template>
+
+<script>
+import Notification from './Notification'
+    export default {
+        components: {
+            Notification
+        },
+        computed: {
+            notificationsData () {
+                return this.$store.getters.notificationsData.slice(0,2)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+
+
+</style>
