11c0155ff3ad12275524956c4a72f02cb870545c
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-28 09:14:17 +0200

    Add tempData to state and create getter

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    tempData: [['16:00:00', 32], ['17:00:00', 38], ['18:00:00', 42], ['19:00:00', 41], ['20:00:00', 45]]
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    tempData (state) {
+      return state.tempData
+    }
   }
 })
