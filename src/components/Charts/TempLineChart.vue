dbc64942d6c267dd288e21378916f124da142fe6
Pawel Peczkowski <peczkowskip@gmail.com>
2018-09-28 09:14:52 +0200

    Get tempData from vuex

--- src/components/Charts/TempLineChart.vue
+++ src/components/Charts/TempLineChart.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
-    <line-chart></line-chart>
+    <line-chart :data="tempData"></line-chart>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data () {
+            return {
+
+            }
+        },
+        computed: {
+            tempData () {
+                return this.$store.getters.tempData
+            }
+        }
         
     }
 </script>
